83850fbfd9d37971aedb0949bf32be71bdb3bdef
syedabuthahirm <abuthahir741@gmail.com>
2021-11-23 22:37:18 +0530

    Base Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import MapView from "@arcgis/core/views/MapView";
+import WebMap from "@arcgis/core/Map";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const mapDiv = useRef(null);
+
+  useEffect(() => {
+    if (mapDiv.current) {
+      const webmap = new WebMap({
+        basemap: 'topo-vector'
+      });
+
+      const view = new MapView({
+        container: mapDiv.current,
+        map: webmap,
+        zoom: 3
+      });
+    }
+  }, []);
+
+  return <div className="mapDiv" ref={mapDiv}></div>;
 }
 
 export default App;
