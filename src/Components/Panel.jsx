7e8fe591904d54dd88a44c8899b2369715b54ce9
syedabuthahirm <abuthahir741@gmail.com>
2021-11-29 19:09:09 +0530

    Panel Css Mockup

---
+++ src/Components/Panel.jsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginRight: -drawerWidth,
+        ...(open && {
+            transition: theme.transitions.create('margin', {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            marginRight: 0,
+        }),
+        overflow: 'auto'
+    }),
+);
+
+export default function PersistentDrawerRight() {
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const toggleDrawer = () => {
+        setOpen(!open);
+    }
+
+    return (
+        <Box sx={{ display: 'flex', height: '100%', overflow: 'hidden' }}>
+            <CssBaseline />
+            <Main open={open}>
+                {Array.from({ length: 10 }, (v, i) => i).map((i) => (
+                    <Typography paragraph>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+                        enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+                        imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+                        Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+                        Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                        adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+                        nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+                        leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+                        feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+                        consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+                        sapien faucibus et molestie ac.
+                    </Typography>
+                ))}
+            </Main>
+            <IconButton onClick={toggleDrawer} sx={{ position: 'absolute', top: '8px', right: open ? drawerWidth : '12px' }}>
+                <MenuIcon />
+            </IconButton>
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                    },
+                }}
+                variant="persistent"
+                anchor="right"
+                open={open}
+            >
+                <Typography>Hello</Typography>
+            </Drawer>
+        </Box>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Panel from './Components/Panel';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Panel />
   </React.StrictMode>,
   document.getElementById('root')
 );
